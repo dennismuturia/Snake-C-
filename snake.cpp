12b7a174d0d182686bc7db7575993f73de3b4b88
dennismuturia <dmuturia2922@gmail.com>
2018-04-08 19:38:02 +0300

    Made a few variables  for the game space as x and y respectively, the meat position, score and the directions of the snake.

--- snake.cpp
+++ snake.cpp
@@ -4,7 +4,15 @@ This is just a basic snake game.
 Remember snake Xenxia?
 Thats it. This programme will not be object oriented
 */
-bool gameOver; //Setting this variable gameover  to boolean 
+bool gameOver;//Setting this variable gameover  to boolean
+
+//Setting the width and height of the area of the game 
+const int width = 20; 
+const int height = 20;
+
+int x, y, meatx, meaty, score; //This variable is responsible for the position of the snake as well as the fruits as well as a score.
+enum eDirection {STOP = 0, RIGHT, UP, DOWN, LEFT};//We might need  the controls to turn the snake in the different directions. Will be using enum
+eDirection dir; //This will hold the direction of the snake.
 void setup(){
     gameOver = true;
 }
