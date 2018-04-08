31926f83fe4a74e8342a1d59ff7e4a176ffdc59b
dennismuturia <dmuturia2922@gmail.com>
2018-04-08 19:13:39 +0300

    I was just outlining the skeleton of the  structure. The programme will compose of 4functions. Each function will carry out a function that will be explained later.

---
+++ snake.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+/*
+This is just a basic snake game.
+Remember snake Xenxia?
+Thats it. This programme will not be object oriented
+*/
+void setup(){
+
+}
+void logic(){
+
+}
+void control(){
+
+}
+void draw(){
+    
+}
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
