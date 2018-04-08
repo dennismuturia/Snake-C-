9aac936bb7a63d2d6efa75997288ae1fa11ef65e
dennismuturia <dmuturia2922@gmail.com>
2018-04-08 19:25:54 +0300

    Initialized the gameover  variable and set it to true in the setup method. Tnen in the main function, declared that when it is false, other variable are initialized and run.

--- snake.cpp
+++ snake.cpp
@@ -4,8 +4,9 @@ This is just a basic snake game.
 Remember snake Xenxia?
 Thats it. This programme will not be object oriented
 */
+bool gameOver; //Setting this variable gameover  to boolean 
 void setup(){
-
+    gameOver = true;
 }
 void logic(){
 
@@ -14,10 +15,16 @@ void control(){
 
 }
 void draw(){
-    
+
 }
 int main(int argc, char const *argv[])
 {
-    
+    setup();
+    //If game over variable is false it should initiate the other defined methods above.
+    while (!gameOver){
+        draw();
+        control();
+        logic();
+    }
     return 0;
 }
